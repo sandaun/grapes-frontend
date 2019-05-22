61b86b6b55136cc53642734c84fdc822de991170
Oriol Carbo <oriol.carbo@gmail.com>
2019-05-22 21:01:30 +0200

    UPDATES FoodList file name

--- src/components/wine/FoodList.js
+++ src/components/wine/FoodList.js
@@ -3,7 +3,7 @@ import { withRouter } from 'react-router-dom';
 import Pairing from "../../lib/wine-service";
 import { Container, Card, Button, Spinner } from 'react-bootstrap';
 import Back from "../BackButton";
-import Recipes from "../wine/foodRecipes";
+import Recipes from "./FoodRecipes";
 
 class FoodList extends Component {
 
